ff567df5c535e84aa1e5f665d47aaf99413394ce
John <jjkiely@gmail.com>
2016-11-26 16:00:49 +0000

    created induction element
    .

--- app.jsx
+++ app.jsx
@@ -3,24 +3,56 @@ const React = window.React
 class App extends React.Component {
     constructor() {
         super();
-        this.state = introduction()
+        this.state = {
+            page: this.start()
+        }
+    }
+
+    start() {
+        return (
+            <Introduction next={() => this.setState({ page: this.showImages }) } />
+        );
+    }
+    
+    showImages() {
+        return (
+            <Induction />
+        );
+    }
+
+    testImage() {
+        return (
+            <p>hi</p>
+        );
     }
 
     render() {
         return (
-            this.state
+            this.state.page
         );
     }
     
 }
 
-function introduction() {
+function Introduction(props) {
     return(
         <div id="introduction">
             <h1>McCollough</h1>
             <p>Explination of the McCollough Effect</p>
             <p>Explination of how to induce it</p>
             <p>Warning</p>
+            <button className="toImages" onClick={() => props.next()}>
+                Start
+            </button>
+        </div>
+    );
+}
+
+function Induction() {
+    return (
+        <div id="induction">
+            <img src="induction1.png"></img>
+            <img src="induction2.png"></img>
         </div>
     );
 }
