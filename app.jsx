ac48128f51f08b2fc70ab448d517949ba5b8e6f2
John <jjkiely@gmail.com>
2016-11-26 14:45:50 +0000

    created introduction element

--- app.jsx
+++ app.jsx
@@ -1,13 +1,28 @@
 const React = window.React
 
 class App extends React.Component {
+    constructor() {
+        super();
+        this.state = introduction()
+    }
 
     render() {
         return (
-            <p>Hello</p>
+            this.state
         );
     }
     
+}
+
+function introduction() {
+    return(
+        <div id="introduction">
+            <h1>McCollough</h1>
+            <p>Explination of the McCollough Effect</p>
+            <p>Explination of how to induce it</p>
+            <p>Warning</p>
+        </div>
+    );
 }
 
 ReactDOM.render(
