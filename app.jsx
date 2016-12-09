8bf3e53d428f5e0c47b245f862acc6a3b73f7de4
John <jjkiely@gmail.com>
2016-12-09 18:50:29 +0000

    using anon function in setTimeout

--- app.jsx
+++ app.jsx
@@ -90,7 +90,7 @@ class Timer extends React.Component {
     }
 
     componentDidMount() {
-        setTimeout(this.setState, 1000, { now: this.getTime() })
+        setTimeout(() => this.setState({ now: this.getTime() }), 1000)
     }
 
     render() {
