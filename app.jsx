eddcced15ffbd4be54cca61d53cc57cb0a98bb23
John <jjkiely@gmail.com>
2016-12-19 16:56:04 +0000

    copy for intro page

--- app.jsx
+++ app.jsx
@@ -40,9 +40,11 @@ function Introduction(props) {
     return (
         <div id="introduction">
             <h1>McCollough</h1>
-            <p>Explination of the McCollough Effect</p>
-            <p>Explination of how to induce it</p>
-            <p>Warning</p>
+            <div id="copy">
+                <p>The <a src="https://en.wikipedia.org/wiki/McCollough_effect">McCullough</a> <a src="http://people.brandeis.edu/~sekuler/SensoryProcessesMaterial/McColloughArticle1965.pdf">(1965)</a> effect is visual phenomenon in which black and white gratings appear colored with one of two colors depending upon their orientation . In order to induce it one must spend several minutes looking at alternating horizontal and vertical gratings, each interspersed with a different (preferably complementary) color.</p>
+                <p>The most notable part of the effect is how long lasting it can be, while an induction period of two to four minutes will result in the effect persisting up to an hour, an induction period of 15 minutes can result in the effect persisting for over three months <a src="http://psycnet.apa.org/journals/xhp/1/4/323/">(Jones, Holding. 1975)</a>.</p>
+                <p>If you're interested in inducing the McCollough effect in yourself just click start below and look directly at the centre of the images for at least two minutes. Since you're going to be here a while you might want to listen to something, might I recommend the Radiolab episode on <a src="http://www.radiolab.org/story/211119-colors/">color?</a></p>
+            </div>
             <button className="toImages" onClick={() => props.next()}>
                 Start
             </button>
